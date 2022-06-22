01d468f752976cb1e00a665aeaf830a157efcbd4
Josh Vince <joshmvince@gmail.com>
2022-06-22 11:53:36 +0000

    Add ssh keychain to save time

--- .zshrc
+++ .zshrc
@@ -1,5 +1,4 @@
-eval `ssh-agent -s`
-eval ssh-add
+eval `keychain --eval --agents ssh id_ed25519`
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
--- newsession.sh
+++ newsession.sh
@@ -19,37 +19,54 @@ check_carwow() {
 credentials() {
   printf "\n🔐 ${bold}Checking credentials...${normal}\n"
 
-  SSH_KEY=LOCATION=/home/vscode/.ssh/if_ed25519
+  SSH_KEY_LOCATION=/home/vscode/.ssh/id_ed25519
+  SSH_PUB_LOCATION=/home/vscode/.ssh/id_ed25519.pub
 
-  if [ -f "$SSH_KEY_LOCATION" ]; then
-    printf "⭐️ Your SSH key exists in this environment!"
+  if [[ -f "$SSH_KEY_LOCATION" && -f "$SSH_PUB_LOCATION" ]]; then
+    printf "✅ Your SSH key exists in this environment!\n"
   else
     printf "💥 Your SSH key is not yet saved to this environment.\n\n"
     printf "${green}gh codespace cp ~/.ssh/id_ed25519 remote:.ssh/${normal}\n"
-    printf "Go ahead and ${bold}run this 👆 command from your host machine${normal} and then come back here."
+    printf "${green}gh codespace cp ~/.ssh/id_ed25519.pub remote:.ssh/${normal}\n"
+    printf "Go ahead and ${bold}run these 👆 commands from your host machine${normal} and then come back here."
 
     read -s -r -p "Once you've done that, press ENTER to continue "
+    printf "✅ Your SSH key exists in this environment!\n"
   fi
+}
 
-  printf "\n${bold}🕵🏾 The SSH agent...${normal}\n"
+keychain() {
+  printf "\n${bold}🔑 The SSH keychain${normal}\n"
 
-  while true; do
-    read -p "Would you like to start the SSH agent and add your passphrase? [y/n] " yn
-    case $yn in
-      [Yy]* ) eval `ssh-agent -s`; ssh-add; break;;
-      [Nn]* ) printf "\n\nNo worries!\n"; break;;
-    esac
-  done
+  if ! command -v keychain &> /dev/null; then
+    printf "\n\n\n\n🛑 keychain was not found.\n\n\n"
+    while true; do
+      read -p "Would you like to install keychain and add your keys? [y/n] " yn
+      case $yn in
+        [Yy]* ) eval `sudo apt-get install keychain`; eval `keychain --eval --agents ssh id_ed25519`; break;;
+        [Nn]* ) printf "\n\nNo worries! You can add this later\n"; break;;
+      esac
+    done
+  else
+    if command -v keychain id_ed25519 &> /dev/null; then
+      printf "⭐️ Your SSH key is already saved to the keychain\n"
+    else
+      printf "💥 The SSH key isn't saved to the keychain yet. Trying to save now...\n\n"
+      eval `keychain --eval --agents ssh id_ed25519`
+    fi
+    printf "✅ Keys sorted\n"
+  fi
 }
 
 heroku() {
   printf "\n👩🏻‍💻 ${bold}Checking for Heroku...${normal}"
 
-  printf "💥 I can't tell if you are logged into Heroku. To be sure, let's log in"
+  printf "💥 I can't tell if you are logged into Heroku. To be sure, let's log in\n"
   printf "${green}bin/carwow codespace heroku-login${normal}\n"
   printf "Go ahead and ${bold}run this 👆 command from your host machine${normal} and then come back here."
 
   read -s -r -p "Once you've done that, press ENTER to continue "
+  printf "✅ Heroku sorted\n"
 }
 
 start_core() {
@@ -62,6 +79,7 @@ start_core() {
       [Nn]* ) printf "\n\nNo worries!\n"; break;;
     esac
   done
+  printf "✅ Carwow core services started\n"
 }
 
 start_es67() {
@@ -74,6 +92,7 @@ start_es67() {
       [Nn]* ) printf "\n\nNo worries!\n"; break;;
     esac
   done
+  printf "✅ Elasticsearch-6-7 started\n"
 }
 
 check_status() {
@@ -86,6 +105,7 @@ check_status() {
 check_carwow
 printf "\n\n👋 ${bold}Welcome to your codespace.${normal} Let's get setup...\n\n"
 credentials
+keychain
 heroku
 start_core
 start_es67
